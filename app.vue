7dc8de99346c365fe55809b0815194e1e63ab969
zhangyajun <1655623164@qq.com>
2022-05-28 14:46:34 +0800

    nuxt app

---
+++ app.vue
@@ -0,0 +1,4 @@
+<template>
+<h1>Hello world!</h1>
+<h2>Hello Nust</h2>
+</template>
---
+++ layouts/blog.vue
@@ -0,0 +1,7 @@
+<template>
+<div>
+    <div>my blog navigation bar here</div>
+    <Nuxt />
+    
+</div>
+</template>
